ec6fc2524008a15a6d99bc6c00aba227fd0bd12a
sulthanallaudeen <sa@sulthanallaudeen.com>
2019-05-02 16:27:23 +0530

    master->Added one signal & Error page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
+var OneSignal = require('onesignal-node');      
+
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Twitter Bot' });
+    res.send({ "success": 1, 'data': req.body });
 });
-/* GET home page. */
+/* Post Tweet */
 router.post('/postTweet', function(req, res, next) {
     console.log(req.body)
     var client = new Twitter({
@@ -29,8 +31,33 @@ router.post('/postTweet', function(req, res, next) {
             res.send({ "success": 1, 'tweet': tweet, 'response': response });
         }
     });
-
-    //res.render('index', { title: 'Express' });
+});
+/* Web Push - One Signal */
+router.post('/webPush', function(req, res, next) {
+    var myClient = new OneSignal.Client({      
+        userAuthKey: req.body.userAuthKey,
+        app: { appAuthKey: req.body.appAuthKey, appId: req.body.appId}
+    });      
+          
+    // we need to create a notification to send      
+    var firstNotification = new OneSignal.Notification({      
+        contents: {      
+            en: req.body.message
+        }      
+    });      
+    // set target users      
+    firstNotification.postBody["included_segments"] = ["Active Users"];
+    firstNotification.postBody["excluded_segments"] = ["Banned Users"];
+    // set notification parameters      
+    // firstNotification.postBody["data"] = {"abc": "123", "foo": "bar"};      
+    //firstNotification.postBody["send_after"] = 'Thu May 02 2019 15:58:00 GMT+0530 (UTC)'; // to send immediate push 
+    myClient.sendNotification(firstNotification, function (err, httpResponse,data) {      
+       if (err) {
+        res.send({ "success": 0, 'error': err });
+       } else {
+           res.send({ "success": 1, 'data': data, code : httpResponse.statusCode });
+       }      
+    });
 });
 
 module.exports = router;
